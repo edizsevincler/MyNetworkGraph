82ae8459591ba90e949fdc103beb650bbd6d9f05
edizsevincler <edizsevincler@hotmail.com>
2026-01-01 19:12:28 +0300

    Core katmanı eklendi ve proje güncellendi

---
+++ MyNetworkGraph/Core/Graph/Graph.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyNetworkGraph.Core.Graph
+{
+    internal class Graph
+    {
+    }
+}
---
+++ MyNetworkGraph/Core/Model/Edge.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyNetworkGraph.Core.Model
+{
+    internal class Edge
+    {
+    }
+}
