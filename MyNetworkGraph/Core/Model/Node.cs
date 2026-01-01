441d7038476ceb2aaa42fd725ebb9e97459fa9de
BiraliNesbuik <147419267+BiraliNesbuik@users.noreply.github.com>
2026-01-01 19:20:46 +0300

    Node.cs

--- MyNetworkGraph/Core/Model/Node.cs
+++ MyNetworkGraph/Core/Model/Node.cs
@@ -1,10 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace MyNetworkGraph.Core.Model
+namespace MyNetworkGraph.Core.Model;
+
+public class Node
 {
-    internal class Node
+    public int Id { get; }
+    public string Label { get; set; }
+
+    // Dinamik ağırlık için özellikler
+    public double Active { get; set; }
+    public double Interaction { get; set; }
+    public double DegreeHint { get; set; }
+
+    // UI koordinatları
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Node(int id, string label, double active, double interaction, double degreeHint)
     {
+        Id = id;
+        Label = label;
+        Active = active;
+        Interaction = interaction;
+        DegreeHint = degreeHint;
     }
+
+    public override string ToString()
+        => $"Node{{id={Id}, label='{Label}'}}";
 }
