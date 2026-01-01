c82e46c544e881b907383f191388288794fd8470
BiraliNesbuik <147419267+BiraliNesbuik@users.noreply.github.com>
2026-01-01 19:18:30 +0300

    WeightCalculator.cs
    Weight Calculator eklendi

--- MyNetworkGraph/Core/Graph/WeightCalculator.cs
+++ MyNetworkGraph/Core/Graph/WeightCalculator.cs
@@ -1,10 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using MyNetworkGraph.Core.Model;
 
-namespace MyNetworkGraph.Core.Graph
+namespace MyNetworkGraph.Core.Graph;
+
+public static class WeightCalculator
 {
-    internal class WeightCalculator
+    // weight(i,j) = 1 / (1 + (Ai-Aj)^2 + (Ei-Ej)^2 + (Bi-Bj)^2)
+    public static double Weight(Node a, Node b)
     {
+        double da = a.Active - b.Active;
+        double di = a.Interaction - b.Interaction;
+        double dd = a.DegreeHint - b.DegreeHint;
+
+        double denom = 1.0 + da * da + di * di + dd * dd;
+        return 1.0 / denom;
     }
 }
